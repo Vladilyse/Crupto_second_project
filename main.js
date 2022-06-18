61f41a64fc0d294ff47062ae84e24c3950d11831
Vladilyse <92036521+Vladilyse@users.noreply.github.com>
2022-06-18 13:18:06 +0300

    Crypto_second

---
+++ main.js
@@ -0,0 +1,29 @@
+let policy = document.getElementById('policy')
+let created_button = document.getElementById('button1')
+    created_button.addEventListener('click', function(){
+        policy.style.display='block'
+    })
+let button2 = document.getElementById('button2')
+    button2.addEventListener('click', function(){
+        policy.style.display='none'
+    })
+
+let gal = document.getElementById('gal_img')
+    button3 = document.getElementById('gal')
+    button3.addEventListener('click',function(){
+        gal_img.style.display='block'
+    })
+    gal_img.addEventListener('click',function(){
+        gal_img.style.display='none'
+    })
+
+let regist = document.getElementById('registr')
+    button4 = document.getElementById('cancel')
+    button4.addEventListener('click',function(){
+        registr.style.display='none'
+    })
+let button5 = document.getElementById('button5')
+    button5.addEventListener('click',function(){
+        registr.style.display='block'
+    })
+
